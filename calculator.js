42d149a0cb075faa9303bdb8e84a8a8a9b6285a0
Matheus Lima <mlbp@cin.ufpe.br>
2022-01-13 18:09:48 -0500

    Creating base for main calculator function

---
+++ calculator.js
@@ -0,0 +1,6 @@
+const calculator = () => {
+    const greet = "Hello! \nSelect the option you wish to use \n1: Sum \n2: Subtraction \n3: Multiplication \n4: Division";
+    const input = prompt(greet);
+    alert(`Your input was ${input}`);
+}
+
